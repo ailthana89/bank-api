d579402a44db4cd5932cc873246646a62631c787
ailthana89 <nathy.biblio@gmail.com>
2021-06-20 10:58:29 -0300

    Primeira parte

---
+++ src/main/kotlin/com/projeto/bankapi/Account.kt
@@ -0,0 +1,15 @@
+package com.projeto.bankapi
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity(name = "accounts")
+data class Account(
+    @Id @GeneratedValue
+    var id: Long? = null,
+    val name: String,
+    val document: String,
+    val phone: String
+
+)
